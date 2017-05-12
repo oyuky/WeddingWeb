edd8c7297ab39f78b60d91bc76f5dd25acb4b0d0
fsalazarnearbpo <fsalazarnearbpo>
2017-05-11 21:10:41 -0500

    Adding invitado.ts

---
+++ src/models/invitado.ts
@@ -0,0 +1,9 @@
+//Invitado Model based on the structure on the wedding web api
+export interface Invitado{
+  invitadoID: number;
+  invitacionID: number;
+  nombre: string;
+  apellidopaterno: string;
+  apellidomaterno: string;
+  menor: number;
+}
