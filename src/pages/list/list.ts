d7a462d5d4e9d980b2c3102826a82b612c45a48f
fsalazarnearbpo <fsalazarnearbpo>
2017-10-14 19:16:25 -0500

    Mesa de Regalos

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -9,23 +9,26 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ListPage {
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{tienda: string, numero: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    this.icons = ['pricetag', 'pricetags'];
 
     this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+    this.items.push({
+        tienda: 'Sears',
+        numero: '#10513921',
+        icon: this.icons[0]
       });
-    }
+
+      this.items.push({
+          tienda: 'Liverpool' ,
+          numero: '#42991151',
+          icon: this.icons[1]
+        });
   }
 
   itemTapped(event, item) {
-  
+
   }
 }
