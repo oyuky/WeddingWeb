afa607eea481eebd94eb843e079799ccd3b2d25d
fsalazarnearbpo <fsalazarnearbpo>
2017-10-20 23:05:12 -0500

    Changes on invitado object

--- src/models/invitado.ts
+++ src/models/invitado.ts
@@ -1,10 +1,11 @@
 //Invitado Model based on the structure on the wedding web api
 export interface Invitado{
   invitadoID: number;
-  invitacionID: number;
+  registro: Date;
+  correo: string;
   nombre: string;
-  apellidopaterno: string;
-  apellidomaterno: string;
+  familia: string;
   menor: number;
   adultos: number;
+  musica: string;
 }
