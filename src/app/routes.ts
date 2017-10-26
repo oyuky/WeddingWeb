6215cc1805dad856b35643aa1a10d0953ce9d0a8
fsalazarnearbpo <fsalazarnearbpo>
2017-10-26 12:11:26 -0500

    fix on package.json and removing routes

--- src/app/routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { IntroPage } from '../pages/intro/intro';
-
-export const appRoutes:Routes = [
-  { path: 'intro/:Id', component: IntroPage },
-  { path: '',redirectTo: '/', pathMatch: 'full' }
-]
